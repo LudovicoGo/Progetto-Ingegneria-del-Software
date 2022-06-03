ed9980c444561678e4eb2e0db1f21ccd4ff96502
Andrea Giampieri <s1093059@studenti.univpm.it>
2022-06-03 19:36:14 +0200

    adeguamento colori e stato

--- RistoMatic/Viste/Blocks/BlockTavolo.py
+++ RistoMatic/Viste/Blocks/BlockTavolo.py
@@ -3,7 +3,7 @@ from random import *
 from PySide6.QtCore import Signal,Slot
 from RistoMatic.Viste.Blocks.BlockDettaglioTavolo import VistaTavolo
 from RistoMatic.Viste.Blocks._BlockElementiComanda import BlockListaElementiComanda
-
+from RistoMatic.GestioneAttivita.Enum import StatoTavolo
 
 class BlockTavolo(QtWidgets.QPushButton):
 
@@ -16,12 +16,12 @@ class BlockTavolo(QtWidgets.QPushButton):
 
         self.setText(f"Tavolo {num} \n Coperti: {randint(0,10)} \n Status: {stat}")
 
-        if (stat == "Libero"):
+        if (stat == StatoTavolo.UTILIZZABILE):
             self.setStyleSheet('QPushButton {background-color: green; font-size: 16px; height: 90; width:150;}')
-        elif (stat == "prenotato"):
+        elif (stat == StatoTavolo.PRENOTATO):
             self.setStyleSheet('QPushButton {background-color: orange; font-size: 16px; height: 90; width:150;}')
             self.setText(self.text() + "\n Nome Cognome \n 3332323232")
-        elif(stat == "occupato"):
+        elif(stat == StatoTavolo.OCCUPATO):
             self.setStyleSheet('QPushButton {background-color: red; font-size: 16px; height: 90; width:150;}')
 
         self.clicked.connect(self.show_detail)
