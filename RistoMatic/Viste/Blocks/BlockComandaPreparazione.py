a4040e5b9064d97765154c98ac9c6d905d5821b0
Andrea Giampieri <s1093059@studenti.univpm.it>
2022-06-03 19:35:29 +0200

    cambiata label in base all'istanza di riferimento di comanda (tavolo,asporto)

--- RistoMatic/Viste/Blocks/BlockComandaPreparazione.py
+++ RistoMatic/Viste/Blocks/BlockComandaPreparazione.py
@@ -3,11 +3,16 @@ from PySide6.QtWidgets import QGroupBox, QPushButton, QVBoxLayout, QLabel
 from PySide6.QtCore import Slot,Signal
 from RistoMatic.GestioneAttivita.Comanda import Comanda
 from RistoMatic.Viste.Blocks.BlockElementoComanda import BlockElementoComanda
+from RistoMatic.GestioneAttivita.Tavolo import Tavolo
 
 class BlockComandaPreparazione(QtWidgets.QGroupBox):
 
     def __init__(self, comanda: Comanda):
-        super().__init__(f"Comanda {comanda.numeroComanda} - Tavolo {comanda.rif.riferimentoTavolo} - {comanda.dataCreazione.strftime('%H:%M')}")
+        if(isinstance(comanda.rif,Tavolo)):
+            super().__init__(f"Comanda {comanda.numeroComanda} - Tavolo {comanda.rif.riferimentoTavolo} - {comanda.dataCreazione.strftime('%H:%M')}")
+        else:
+            super().__init__(f"Comanda {comanda.numeroComanda} - Asporto {comanda.rif.riferimentoTavolo} - {comanda.dataCreazione.strftime('%H:%M')}")
+            self.setStyleSheet("QGroupBox {background-color: blue;}")
         self.vbox = QVBoxLayout()
         self.vbox.addStretch(1)
 
