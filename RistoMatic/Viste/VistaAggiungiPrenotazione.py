40e28df9828a67f067db09df9129d1ed2228ad7c
Ludovico Gorgoglione <S1099130@studenti.univpm.it>
2022-06-14 01:47:25 +0200

    update VistaAggiungiPrenotazione

--- RistoMatic/Viste/VistaAggiungiPrenotazione.py
+++ RistoMatic/Viste/VistaAggiungiPrenotazione.py
@@ -1,15 +1,16 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from PySide6 import QtCore
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QCheckBox
 
+from RistoMatic.GestioneAttivita.StatoSala import StatoSala
 from RistoMatic.GestioneAttivita.Cliente import Cliente
 from RistoMatic.GestioneAttivita.Prenotazione import Prenotazione
 
 class VistaAggiungiPrenotazione(QWidget):
 
-    def __init__(self, callback, lista):
+    def __init__(self, callback):
         super(VistaAggiungiPrenotazione, self).__init__()
 
         self.callback = callback
-        self.lista = lista
         self.vLayout = QVBoxLayout()
         self.qlines = {}
         self.addInfoText("nome", "Nome")
@@ -18,13 +19,19 @@ class VistaAggiungiPrenotazione(QWidget):
         self.addInfoText("riferimentoTavolo", "Riferimento Tavolo")
         self.addInfoText("recapitoTelefonico", "Recapito Telefonico")
 
+        self.box = QCheckBox("Da confermare", self)
+        self.box.stateChanged.connect(self.clickBox)
+        self.vLayout.addWidget(self.box)
+
+        self.statoPrenotazione = "Confermata"
+
         okButton = QPushButton("OK")
         prenotazione = okButton.clicked.connect(self.aggiungiPrenotazione)
         self.qlines["okButton"] = okButton
         self.vLayout.addWidget(okButton)
         self.setLayout(self.vLayout)
 
-    def addInfoText(self,nome, label):
+    def addInfoText(self, nome, label):
         self.vLayout.addWidget(QLabel(label))
         testo = QLineEdit(self)
         self.qlines[nome] = testo
@@ -44,11 +51,26 @@ class VistaAggiungiPrenotazione(QWidget):
         recapitoTelefonico = self.qlines["recapitoTelefonico"].text()
         dataPrenotazione = self.qlines["dataPrenotazione"].text()
         numeroPersone = int(self.qlines["numeroPersone"].text())
+        statoPrenotazione = self.statoPrenotazione
+
+
+
         prenotazione.setDataPrenotazione(dataPrenotazione)
         prenotazione.setRiferimentoTavolo(riferimentoTavolo)
         prenotazione.setNumeroPersone(numeroPersone)
+        prenotazione.setStatoPrenotazione(statoPrenotazione)
         prenotazione.cliente.setNomeCliente(nome)
         prenotazione.cliente.setRecapitoTelefonico(recapitoTelefonico)
 
-        self.lista.append(prenotazione)
+        StatoSala.Prenotazioni.append(prenotazione)
         self.close()
+
+    def clickBox(self, state):
+
+        if state == QtCore.Qt.Checked:
+            print('Da confermare')
+            self.statoPrenotazione = "Non Confermata"
+
+        else:
+            print('Da confermare')
+            self.statoPrenotazione = "Confermata"
