510979a5c4d8db04e70f19169e585099feef2344
Ludovico Gorgoglione <S1099130@studenti.univpm.it>
2022-06-20 15:50:20 +0200

    Aggiustamenti

---
+++ RistoMatic/Tests/TestPrenotazione.py
@@ -0,0 +1,24 @@
+import datetime
+import unittest
+
+from RistoMatic.GestioneAttivita.Cliente import Cliente
+from RistoMatic.GestioneAttivita.Prenotazione import Prenotazione
+
+class TestComanda(unittest.TestCase):
+
+    def testCreazionePrenotazione(self):
+        data = datetime.datetime(2022, 6, 14, 19, 30, 0)
+        data.now()
+        cliente = Cliente("Angelo Rossi", "3334445556")
+        prenotazione = Prenotazione(data, 4, "Da confermare", cliente, 2)
+
+        prenotazioni = [prenotazione]
+
+
+        self.assertEqual(cliente.nomeCliente, prenotazioni[0].cliente.nomeCliente)
+        self.assertEqual(cliente.recapitoTelefonico, prenotazioni[0].cliente.recapitoTelefonico)
+        self.assertEqual(prenotazione.id, prenotazioni[0].id)
+        self.assertEqual(prenotazione.dataPrenotazione, prenotazioni[0].dataPrenotazione)
+        self.assertEqual(prenotazione.statoPrenotazione, prenotazioni[0].statoPrenotazione)
+        self.assertEqual(prenotazione.numeroPersone, prenotazioni[0].numeroPersone)
+        self.assertEqual(prenotazione.riferimentoTavolo, prenotazioni[0].riferimentoTavolo)
