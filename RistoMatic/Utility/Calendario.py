db52f2d3d405fffa61c86a51c66ab56066a06714
lucabellantee <s1097955@studenti.univpm.it>
2022-06-27 12:42:33 +0200

    ancora modifiche

--- RistoMatic/Utility/Calendario.py
+++ RistoMatic/Utility/Calendario.py
@@ -1,62 +1,57 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QCalendarWidget, QPushButton, \
-							QHBoxLayout, QVBoxLayout
+    QHBoxLayout, QVBoxLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QTextCharFormat, QIcon
 import pandas as pd
 
+
 class Calendario(QCalendarWidget):
-	def __init__(self):
-		super().__init__()
-		self.from_date = None
-		self.to_date = None
+    def __init__(self):
+        super().__init__()
+        self.from_date = None
+        self.to_date = None
 
-		self.highlighter_format = QTextCharFormat()
-		# get the calendar default highlight setting
-		self.highlighter_format.setBackground(self.palette().brush(QPalette.Highlight))
-		self.highlighter_format.setForeground(self.palette().color(QPalette.HighlightedText))
+        self.highlighter_format = QTextCharFormat()
+        # get the calendar default highlight setting
+        self.highlighter_format.setBackground(self.palette().brush(QPalette.Highlight))
+        self.highlighter_format.setForeground(self.palette().color(QPalette.HighlightedText))
 
+        # this will pass selected date value as a QDate object
+        self.clicked.connect(self.select_range)
 
-		# this will pass selected date value as a QDate object
-		self.clicked.connect(self.select_range)
+        super().dateTextFormat()
 
-		super().dateTextFormat()
+    def highlight_range(self, format):
+        if self.from_date and self.to_date:
+            d1 = min(self.from_date, self.to_date)
+            d2 = max(self.from_date, self.to_date)
+            while d1 <= d2:
+                self.setDateTextFormat(d1, format)
+                d1 = d1.addDays(1)
 
-	def highlight_range(self, format):
-		if self.from_date and self.to_date:
-			d1 = min(self.from_date, self.to_date)
-			d2 = max(self.from_date, self.to_date)
-			while d1 <= d2:
-				self.setDateTextFormat(d1, format)
-				d1 = d1.addDays(1)
+    def select_range(self, date_value):
+        self.highlight_range(QTextCharFormat())
 
-	def select_range(self, date_value):
-		self.highlight_range(QTextCharFormat())
-
-		# check if a keyboard modifer is pressed
-		if QApplication.instance().keyboardModifiers() & Qt.ShiftModifier and self.from_date:
-			self.to_date = date_value
-			# print(self.from_date, self.to_date)
-			self.highlight_range(self.highlighter_format)
-		else:
-			# required
-			self.from_date = date_value
-			self.to_date = None
-			# print(self.from_date, self.to_date, 'x')
-
+        # check if a keyboard modifer is pressed
+        if QApplication.instance().keyboardModifiers() & Qt.ShiftModifier and self.from_date:
+            self.to_date = date_value
+            # print(self.from_date, self.to_date)
+            self.highlight_range(self.highlighter_format)
+        else:
+            # required
+            self.from_date = date_value
+            self.to_date = None
+        # print(self.from_date, self.to_date, 'x')
 
 ## FINE APP
 
 
+# if __name__ == '__main__':
+# don't auto scale when drag app to a different monitor.
+# QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
 
+# app = QApplication(sys.argv)
 
-#if __name__ == '__main__':
-	# don't auto scale when drag app to a different monitor.
-	# QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
-
-	#app = QApplication(sys.argv)
-
-	#myApp = MyApp()
-	#myApp.show()
-
-
+# myApp = MyApp()
+# myApp.show()
