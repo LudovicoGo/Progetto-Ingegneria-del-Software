88456ce16e25aa45a5909b195dc896e0a4757c3a
Andrea Giampieri <s1093059@studenti.univpm.it>
2022-07-24 15:58:25 +0200

    fix vista preparazione

--- RistoMatic/Viste/Blocks/BlockComandaPreparazione.py
+++ RistoMatic/Viste/Blocks/BlockComandaPreparazione.py
@@ -2,6 +2,7 @@ from PySide6 import QtCore, QtWidgets
 from PySide6.QtWidgets import QGroupBox, QPushButton, QVBoxLayout, QLabel
 from PySide6.QtCore import Slot,Signal
 from RistoMatic.GestioneAttivita.Comanda import Comanda
+from RistoMatic.GestioneAttivita.Enum import Zone
 from RistoMatic.Viste.Blocks.BlockElementoComandaPreparazione import BlockElementoComandaPreparazione
 from RistoMatic.GestioneAttivita.Tavolo import Tavolo
 
@@ -16,11 +17,8 @@ class BlockComandaPreparazione(QtWidgets.QGroupBox):
         self.vbox = QVBoxLayout()
         self.vbox.addStretch(1)
 
-# TODO CAPIRE PERCHE NON VA
         for elem in comanda.elementiComanda:
-         #  print(elem.elemento.areaPreparazione==1)
-        #   if elem.elemento.areaPreparazione==1 or elem.elemento.areaPreparazione==3:
-               if not elem.getIsPronta():
+               if not elem.getIsPronta() and elem.elemento.getAreaPreparazione()==Zone.CUCINA:
                   btn = BlockElementoComandaPreparazione(elem)
                   self.vbox.addWidget(btn)
 
